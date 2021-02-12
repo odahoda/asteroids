11d2d2fa61e71b7ebcf270aadfb128492d02a199
Ben Niemann <pink@odahoda.de>
2021-02-13 00:29:26 +0100

    Dynamically instantiate main_menu and level objects.

--- game.gd
+++ game.gd
@@ -1,46 +1,44 @@
 extends Node2D
 
-var game_state = 'menu'
-var next_state = null
-onready var main_menu = $HUD/main_menu
-onready var level = $level
-onready var animations = $animations
+var level_scene = preload("res://level.tscn")
+var level = null
+var main_menu_scene = preload("res://main_menu.tscn")
+var main_menu = null
 
 func _ready() -> void:
 	randomize()
-	main_menu.connect('quit_pressed', self, 'quit_game')
-	main_menu.connect('play_pressed', self, 'start_level')
-	level.connect('finished', self, 'level_finished')
-	animations.connect('animation_finished', self, 'animation_finished')
-
 	set_state('menu')
 
 func set_state(state: String) -> void:
-	game_state = state
 	if state == 'menu':
-		main_menu.visible = true
-		level.visible = false
+		main_menu = main_menu_scene.instance()
+		main_menu.connect('quit_pressed', self, 'quit_game')
+		main_menu.connect('play_pressed', self, 'start_level')
+		add_child(main_menu)
+
 	elif state == 'play':
-		main_menu.visible = false
-		level.visible = true
+		main_menu.queue_free()
+		main_menu = null
+
+		level = level_scene.instance()
+		level.connect('finished', self, 'level_finished')
+		add_child(level)
+
 	elif state == 'quit':
-		main_menu.visible = false
-		level.visible = false
+		main_menu.queue_free()
+		main_menu = null
 		get_tree().quit()
+
 	else:
 		print("huh?")
 
-func animation_finished(anim) -> void:
-	animations.seek(0, true)
-	set_state(next_state)
-		
 func quit_game() -> void:
-	next_state = 'quit'
-	animations.play('menu_out')
+	main_menu.fade_out(self, 'set_state', ['quit'])
 	
 func start_level() -> void:
-	next_state = 'play'
-	animations.play('menu_out')
+	main_menu.fade_out(self, 'set_state', ['play'])
 
 func level_finished() -> void:
+	level.queue_free()
+	level = null
 	set_state('menu')
--- main_menu.gd
+++ main_menu.gd
@@ -1,13 +1,17 @@
-extends Control
+extends CanvasLayer
 
 signal quit_pressed
 signal settings_pressed
 signal play_pressed
+onready var quit_button = $controls/buttons/quit
+onready var play_button = $controls/buttons/play
+onready var settings_button = $controls/buttons/settings
 
 func _ready() -> void:
-	$buttons/quit.connect('button_up', self, '__quit_pressed')
-	$buttons/play.connect('button_up', self, '__play_pressed')
-	$buttons/settings.connect('button_up', self, '__settings_pressed')
+	quit_button.connect('button_up', self, '__quit_pressed')
+	play_button.connect('button_up', self, '__play_pressed')
+	settings_button.connect('button_up', self, '__settings_pressed')
+	$animations.play('fade_in')
 
 func __quit_pressed() -> void:
 	emit_signal('quit_pressed')
@@ -17,3 +21,10 @@ func __play_pressed() -> void:
 	
 func __settings_pressed() -> void:
 	emit_signal('settings_pressed')
+
+func fade_out(cb_obj, cb_func, cb_args=[]) -> void:
+	$animations.play('fade_out')
+	$animations.connect('animation_finished', self, '__fade_out_finished', [cb_obj, cb_func, cb_args], CONNECT_ONESHOT)
+
+func __fade_out_finished(_anim, cb_obj, cb_func, cb_args) -> void:
+	cb_obj.callv(cb_func, cb_args)
