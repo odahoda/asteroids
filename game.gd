d4c5a9b7bddf1d0e82fcacd5fd60d8038cff3ff0
Ben Niemann <pink@odahoda.de>
2021-03-10 19:02:13 +0100

    Initial settings screen. Still buggy.

--- game.gd
+++ game.gd
@@ -6,9 +6,15 @@ var main_menu_scene = preload("res://main_menu.tscn")
 var main_menu = null
 var hall_of_fame_scene = preload("res://hall_of_fame.tscn")
 var hall_of_fame = null
+onready var settings = $settings
 
 func _ready() -> void:
 	randomize()
+
+	$settings.visible = false
+	$settings.connect('done', self, 'settings_finished')
+	$settings.load_settings()
+
 	set_state('menu')
 
 func set_state(state: String) -> void:
@@ -16,6 +22,7 @@ func set_state(state: String) -> void:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		main_menu = main_menu_scene.instance()
 		main_menu.connect('quit_pressed', self, 'quit_game')
+		main_menu.connect('settings_pressed', self, 'show_settings')
 		main_menu.connect('play_pressed', self, 'start_level')
 		main_menu.connect('highscores_pressed', self, 'show_highscores')
 		add_child(main_menu)
@@ -28,6 +35,10 @@ func set_state(state: String) -> void:
 		hall_of_fame.connect('closed', self, 'highscores_finished')
 		add_child(hall_of_fame)
 
+	elif state == 'settings':
+		$settings.visible = true
+		$settings.fade_in()
+	
 	elif state == 'play':
 		main_menu.queue_free()
 		main_menu = null
@@ -48,13 +59,25 @@ func quit_game() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	main_menu.fade_out(self, 'set_state', ['quit'])
 	
-func start_level() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-	main_menu.fade_out(self, 'set_state', ['play'])
-
 func show_highscores() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	main_menu.fade_out(self, 'set_state', ['highscores'])
+
+func highscores_finished() -> void:
+	hall_of_fame.queue_free()
+	hall_of_fame = null
+	set_state('menu')
+
+func show_settings() -> void:
+	main_menu.fade_out(self, 'set_state', ['settings'])
+
+func settings_finished() -> void:
+	$settings.visible = false
+	set_state('menu')
+
+func start_level() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	main_menu.fade_out(self, 'set_state', ['play'])
 
 func level_finished() -> void:
 	var score = level.score
@@ -67,8 +90,3 @@ func level_finished() -> void:
 		add_child(hall_of_fame)
 	else:
 		set_state('menu')
-
-func highscores_finished() -> void:
-	hall_of_fame.queue_free()
-	hall_of_fame = null
-	set_state('menu')
---
+++ settings.gd
@@ -0,0 +1,97 @@
+extends Control
+
+signal done
+
+var settings = null
+
+onready var done_button = $controls/done
+onready var fullscreen_button = $controls/grid/fullscreen
+onready var music_volume_slider = $controls/grid/music_volume
+onready var sound_volume_slider = $controls/grid/sound_volume
+
+func _ready() -> void:
+	done_button.connect('button_up', self, 'fade_out')
+	fullscreen_button.connect('toggled', self, '__fullscreen_changed')
+	music_volume_slider.connect('value_changed', self, '__music_volume_changed')
+	sound_volume_slider.connect('value_changed', self, '__sound_volume_changed')
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		done_button.grab_focus()
+		fade_out()
+
+func fade_in() -> void:
+	$animations.play('fade_in')
+	$animations.seek(0, true)
+
+func fade_out() -> void:
+	$animations.play('fade_out')
+	$animations.connect('animation_finished', self, '__fade_out_finished', [], CONNECT_ONESHOT)
+
+func load_settings() -> void:
+	var fp = File.new()
+	if fp.file_exists("user://settings.json"):
+		fp.open("user://settings.json", File.READ)
+		settings = parse_json(fp.get_as_text())
+		fp.close()
+
+	else:
+		settings = {
+			'fullscreen': false,
+			'music_volume': 100,
+			'sound_volume': 100,
+		}
+
+	fullscreen_button.pressed = settings['fullscreen']
+	__apply_fullscreen()
+
+	music_volume_slider.value = settings['music_volume']
+	__apply_music_volume()
+
+	sound_volume_slider.value = settings['sound_volume']
+	__apply_sound_volume()
+
+func save_settings() -> void:
+	var fp = File.new()
+	fp.open("user://settings.json", File.WRITE)
+	fp.store_string(to_json(settings))
+	fp.close()
+
+func __fade_out_finished(_anim) -> void:
+	save_settings()
+	emit_signal('done')
+
+func __fullscreen_changed(value) -> void:
+	settings['fullscreen'] = value
+	__apply_fullscreen()
+	
+func __apply_fullscreen() -> void:
+	OS.window_fullscreen = settings['fullscreen']
+
+func __music_volume_changed(value) -> void:
+	settings['music_volume'] = value
+	__apply_music_volume()
+	
+func __apply_music_volume() -> void:
+	var bus_idx = AudioServer.get_bus_index('music')
+	var value = settings['music_volume']
+	if value == 0:
+		AudioServer.set_bus_mute(bus_idx, true)
+	else:
+		var value_db = 10 * log(value / 100)
+		AudioServer.set_bus_mute(bus_idx, false)
+		AudioServer.set_bus_volume_db(bus_idx, value_db)
+
+func __sound_volume_changed(value) -> void:
+	settings['sound_volume'] = value
+	__apply_sound_volume()
+
+func __apply_sound_volume() -> void:
+	var bus_idx = AudioServer.get_bus_index('sfx')
+	var value = settings['sound_volume']
+	if value == 0:
+		AudioServer.set_bus_mute(bus_idx, true)
+	else:
+		var value_db = 10 * log(value / 100)
+		AudioServer.set_bus_mute(bus_idx, false)
+		AudioServer.set_bus_volume_db(bus_idx, value_db)
